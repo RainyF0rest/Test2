30d23afde03624a7c6f6d88641c820ec024787e6
RainyF0rest <107770951+RainyF0rest@users.noreply.github.com>
2022-10-19 19:00:10 +0800

    stationName

--- testt/index.js
+++ testt/index.js
@@ -1,10 +1,16 @@
 async function getInfo(){
     const CarData = await fetch("https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php?line=AEL&sta=HOK" )
     const Json = await CarData.json()
-    console.log(Json)
+    console.log(Json)    
+    const STA_NAME={
+AWE:"博覧館",
+HOK:"香港"
+
+    }
+
     const Info = Json.data["AEL-HOK"].UP
     document.getElementById("msg").innerHTML=
-    `<div>${Info[0].dest} ${Info[0].plat} ${Info[0].time}
+    `<div>${STA_NAME[Info[0].dest]} ${Info[0].plat} ${Info[0].time}
     </div>`}
 
 
