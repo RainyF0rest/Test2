3c88919d32bce23a8b556fc3c0304e5f45b2221a
RainyF0rest <107770951+RainyF0rest@users.noreply.github.com>
2022-10-20 19:12:07 +0800

    Just Now Code

--- testt/index.js
+++ testt/index.js
@@ -5,13 +5,16 @@ async function getInfo(){
     const STA_NAME={
 AWE:"博覧館",
 HOK:"香港"
-
     }
+let now=Json.curr_time
 
     const Info = Json.data["AEL-HOK"].UP
     document.getElementById("msg").innerHTML=
     `<div>${STA_NAME[Info[0].dest]} ${Info[0].plat} ${Info[0].time}
-    </div>`}
+    </div>
+    <div>${STA_NAME[Info[1].dest]} ${Info[1].plat} ${Info[1].time}
+    </div>`
+}
 
 
 getInfo();
