f249be1fb6fa71913067697310812776b6047165
RainyF0rest <107770951+RainyF0rest@users.noreply.github.com>
2022-10-25 00:22:11 +0800

    BigChange
    due to avoid error

--- testt/index.js
+++ testt/index.js
@@ -9,44 +9,35 @@ async function getInfo(){
 AWE:"博覧館",
 HOK:"香港"
     }
-//translate Time>TimeStamp>How much time left
-    const Info = Json.data["AEL-HOK"].UP
- const now=Json.curr_time
-    const nowTimeStamp=new Date(now).getTime();
+//QuickPath
+const Info=Json.data["AEL-HOK"].UP
+const currTime=Json.curr_time;
+const timeNow=new Date(currTime)
+console.log(timeNow)
 
-const train1Time=Info[0].time
-const train1TimeStamp=new Date(train1Time).getTime();
+let result=`<div>${timeNow}</div>`
 
-const nextTrain1=Math.ceil((train1TimeStamp-nowTimeStamp)/1000/60);
+for(let eachTrain of Info){
+result+=`<div>${STA_NAME[eachTrain.dest]}  ${eachTrain.plat}  ${calTTNT(currTime,eachTrain.time)}</div>`
 
-//automatic the process of calculate instead of writing code for every train
-function getTimeDifferent(curr,next){
-    const nowTimeStamp=new Date(curr).getTime();
-    const train1TimeStamp=new Date(next).getTime();
-    const nextTrain1=Math.ceil((train1TimeStamp-nowTimeStamp)/1000/60);
-
-if (nextTrain1 === 1 ){
-return `即將到達`}
-else if (nextTrain1 <= 0 ){
-return `稍微延遲`}
-else {
-    return `${nextTrain1}分鐘`
-}
-}
-//Write=/=CallFunction , we can call the fc out now
-const train1TimeDif=getTimeDifferent(now,Info[0].time)
-const train2TimeDif=getTimeDifferent(now,Info[1].time)
-
-
-
-//put the words back into the HTML
-    document.getElementById("msg").innerHTML=
-    `<div>${STA_NAME[Info[0].dest]} ${Info[0].plat} ${train1TimeDif}
-    </div>
-    <div>${STA_NAME[Info[1].dest]} ${Info[1].plat} ${train2TimeDif}
-    </div>`
-   
 }
 
+    //put words into HTML
+document.getElementById("msg").innerHTML=result
 
+
+//get Time > TimeStamp>calculate
+function calTTNT(curr,next){
+const ttnt1 = Math.ceil((new Date(next).getTime()-new Date(curr).getTime())/1000/60)
+if (ttnt1===1){
+return "即將到達"
+}
+else if(ttnt1<=0){
+return "正在開出"
+}else{
+return `${ttnt1}分鐘`
+}
+}
+
+}
 getInfo();
